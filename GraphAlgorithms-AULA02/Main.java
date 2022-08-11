3e435b28f5db9479a92101b92334de08919a0374
Arthur Mendonça Feu <72267112+ArthurFeu@users.noreply.github.com>
2022-08-10 21:06:57 -0300

    Add files via upload

---
+++ GraphAlgorithms-AULA02/Main.java
@@ -0,0 +1,18 @@
+class Main {
+  public static void main(String[] args) {
+    Graph g1 = new Graph(4);
+    g1.addEdge(0, 1, 1);
+    g1.addEdge(1, 0, 1);
+    g1.addEdge(0, 3, 1);
+    g1.addEdge(3, 0, 1);
+
+    g1.addEdge(3, 4, 2);
+    g1.addEdge(2, 1, 0);
+
+    System.out.println(g1.toString());
+    System.out.println("Degree node 3: " + g1.degree(3));
+    System.out.println("Highest degree in the graph: " + g1.highestDegree());
+    System.out.println("Lowest degree in the graph: " + g1.lowestDegree());
+    System.out.println("\n" + g1.complement().toString());
+  }
+}
